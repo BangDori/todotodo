66521c3c59d4b534d73967caeb96c884b141eef3
BangDori <qudwns8865@naver.com>
2025-06-11 23:22:43 +0900

    feat: Tailwind css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
 export function App() {
-  return <div>Home</div>;
+  return (
+    <div className="absolute inset-0 flex items-center justify-center">
+      <div className="flex items-center justify-center w-36 h-36 border rounded-md">
+        <p>Hello World</p>
+      </div>
+    </div>
+  );
 }
