5d2270066e701e8dd602f6812010fb25114326b9
BangDori <qudwns8865@naver.com>
2025-06-12 20:19:28 +0900

    feat: 투두 리스트 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { RootLayout } from "./components/layout/RootLayout";
-import { TodoHeader } from "./domains/todo/components/TodoHeader";
+import { TodoHeader, TodoList } from "./domains/todo";
 
 export function App() {
+  /**
+   * @todo 임시 데이터
+   */
+  const todos = [
+    { id: 1, text: "test", checked: false },
+    { id: 2, text: "test2", checked: true },
+  ];
+
   return (
     <RootLayout>
       <TodoHeader />
+      <div className="mt-8">
+        <TodoList todos={todos} />
+      </div>
     </RootLayout>
   );
 }
