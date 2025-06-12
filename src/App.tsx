a7651fdefafe5eb81f7162bf05b29631a676779a
BangDori <qudwns8865@naver.com>
2025-06-12 19:27:56 +0900

    feat: 투두 헤더 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,10 @@
 import { RootLayout } from "./components/layout/RootLayout";
-import { Button } from "./components/ui/button";
-import { Checkbox } from "./components/ui/checkbox";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "./components/ui/dialog";
-import { Input } from "./components/ui/input";
-import { Label } from "./components/ui/label";
+import { TodoHeader } from "./domains/todo/components/TodoHeader";
 
 export function App() {
   return (
     <RootLayout>
-      <div className="flex flex-col gap-4">
-        <Input placeholder="Enter your name" />
-        <Button>Click me!</Button>
-        <div className="flex items-center gap-2">
-          <Checkbox id="test" />
-          <Label htmlFor="test">Check Me!</Label>
-        </div>
-        <Dialog>
-          <DialogTrigger>Open</DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Dialog Title</DialogTitle>
-              <DialogDescription>Dialog Description</DialogDescription>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
-      </div>
+      <TodoHeader />
     </RootLayout>
   );
 }
---
+++ src/domains/todo/components/TodoHeader.tsx
@@ -0,0 +1,8 @@
+export function TodoHeader() {
+  return (
+    <header className="grid gap-2 text-center">
+      <h3 className="text-2xl font-bold">TodoList</h3>
+      <p className="text-sm text-gray-500">할 일을 추가하고 관리해보세요!</p>
+    </header>
+  );
+}
