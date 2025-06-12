76ff1991121d6639c59ab919c2f966906f552720
BangDori <qudwns8865@naver.com>
2025-06-13 01:13:21 +0900

    feat: 투두리스트 에러 바운더리 적용

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,20 @@ import {
   TodoListFetcher,
   TodoListSkeleton,
 } from "./domains/todo";
+import { ApiErrorBoundary } from "./components/error-boundary/ApiErrorBoundary";
 
 export function App() {
   return (
     <RootLayout>
       <TodoHeader />
       <div className="mt-8">
-        <Suspense fallback={<TodoListSkeleton />}>
-          <TodoListFetcher>
-            <TodoList />
-          </TodoListFetcher>
-        </Suspense>
+        <ApiErrorBoundary>
+          <Suspense fallback={<TodoListSkeleton />}>
+            <TodoListFetcher>
+              <TodoList />
+            </TodoListFetcher>
+          </Suspense>
+        </ApiErrorBoundary>
       </div>
     </RootLayout>
   );
