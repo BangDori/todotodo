ced2b1990f27dcb35eecbeb6044e0c01e5631587
BangDori <qudwns8865@naver.com>
2025-06-13 00:54:40 +0900

    feat: 루트 에러바운더리 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/globals.css";
 import { App } from "./App.tsx";
+import { RootErrorBoundary } from "./components/error-boundary";
 import { QueryProvider } from "./queries/provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryProvider>
-      <App />
-    </QueryProvider>
+    <RootErrorBoundary>
+      <QueryProvider>
+        <App />
+      </QueryProvider>
+    </RootErrorBoundary>
   </StrictMode>
 );
