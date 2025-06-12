9741896966f4096f0f1ab1c62b96444722831ae3
BangDori <qudwns8865@naver.com>
2025-06-12 23:01:37 +0900

    feat: 데이터 페칭 시 TodoList 스켈레톤 ui 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import { Suspense } from "react";
 import { RootLayout } from "./components/layout/RootLayout";
-import { TodoHeader, TodoList, TodoListFetcher } from "./domains/todo";
+import {
+  TodoHeader,
+  TodoList,
+  TodoListFetcher,
+  TodoListSkeleton,
+} from "./domains/todo";
 
 export function App() {
   return (
     <RootLayout>
       <TodoHeader />
       <div className="mt-8">
-        <TodoListFetcher>
-          <TodoList />
-        </TodoListFetcher>
+        <Suspense fallback={<TodoListSkeleton />}>
+          <TodoListFetcher>
+            <TodoList />
+          </TodoListFetcher>
+        </Suspense>
       </div>
     </RootLayout>
   );
--- src/domains/todo/index.ts
+++ src/domains/todo/index.ts
@@ -1,4 +1,5 @@
 export { TodoHeader } from "./components/TodoHeader";
+export { TodoListSkeleton } from "./components/TodoListSkeleton";
 export { TodoList } from "./components/TodoList";
 
 export { TodoListFetcher } from "./context/TodoListFetcher";
