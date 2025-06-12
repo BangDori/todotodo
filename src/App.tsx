39c2765c2839553bbf7a94fae1c6cee124da1965
BangDori <qudwns8865@naver.com>
2025-06-12 22:54:18 +0900

    feat: 데이터 페칭 컨텍스트을 적용하여 비즈니스 로직과 ui 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import { RootLayout } from "./components/layout/RootLayout";
-import { TodoHeader, TodoList } from "./domains/todo";
+import { TodoHeader, TodoList, TodoListFetcher } from "./domains/todo";
 
 export function App() {
-  /**
-   * @todo 임시 데이터
-   */
-  const todos = [
-    { id: 1, text: "test", checked: false },
-    { id: 2, text: "test2", checked: true },
-  ];
-
   return (
     <RootLayout>
       <TodoHeader />
       <div className="mt-8">
-        <TodoList todos={todos} />
+        <TodoListFetcher>
+          <TodoList />
+        </TodoListFetcher>
       </div>
     </RootLayout>
   );
