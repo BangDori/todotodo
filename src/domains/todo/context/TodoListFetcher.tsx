4751f227a16df35dee7546a5be43f264e35f9229
BangDori <qudwns8865@naver.com>
2025-06-13 00:56:03 +0900

    feat: 명시적으로 에러를 던지는 경우

--- src/domains/todo/context/TodoListFetcher.tsx
+++ src/domains/todo/context/TodoListFetcher.tsx
@@ -2,7 +2,7 @@ import { createContext, useContext, type PropsWithChildren } from "react";
 import { useGetTodos } from "../api/useGetTodos";
 import type { Todo } from "../types/todo";
 
-const TodoListContext = createContext<Todo[]>([]);
+const TodoListContext = createContext<Todo[] | undefined>(undefined);
 
 export function TodoListFetcher({ children }: PropsWithChildren) {
   const { data: todos = [] } = useGetTodos();
@@ -14,12 +14,17 @@ export function TodoListFetcher({ children }: PropsWithChildren) {
   );
 }
 
-// ⚠️ useTodoList 훅은 TodoListFetcher 컴포넌트 내부에서만 사용하여야 합니다!
+/**
+ * ⚠️ useTodoList 훅은 TodoListFetcher 컴포넌트 내부에서만 사용하여야 합니다!
+ * 외부에서 사용할 경우, todos가 undefined 값을 가지게 되고 에러가 발생시킵니다.
+ */
 export function useTodoList() {
   const todos = useContext(TodoListContext);
 
-  if (!todos) {
-    throw new Error("useTodoList must be used within a TodoListFetcher");
+  if (todos === undefined) {
+    throw new Error(
+      "useTodoList는 TodoListFetcher 내부에서만 사용할 수 있어요."
+    );
   }
 
   return todos;
