b01645261a02b6e6dc887966e2abfaa3587cf3b7
BangDori <qudwns8865@naver.com>
2025-06-13 14:09:00 +0900

    test: 입력값 초기화 테스트

--- src/domains/todo/__test__/TodoForm.spec.tsx
+++ src/domains/todo/__test__/TodoForm.spec.tsx
@@ -2,10 +2,12 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import { TodoForm } from "../components/TodoForm";
 
 describe("할 일 추가 폼 입력 테스트", () => {
-  it("입력값이 1글자 미만이면 추가하기 버튼이 비활성화된다", () => {
+  beforeEach(() => {
     // given: 할 일 추가 폼이 렌더링된 상태에서
     render(<TodoForm />);
+  });
 
+  it("입력값이 1글자 미만이면 추가하기 버튼이 비활성화된다", () => {
     // when: 입력값이 비어 있을 때
     const input = screen.getByTestId("todo-input");
     const button = screen.getByTestId("todo-add-button");
@@ -16,9 +18,6 @@ describe("할 일 추가 폼 입력 테스트", () => {
   });
 
   it("입력값이 1글자 이상이면 추가하기 버튼이 활성화된다", () => {
-    // given: 할 일 추가 폼이 렌더링된 상태에서
-    render(<TodoForm />);
-
     // when: 입력값이 1글자 이상일 때
     const input = screen.getByTestId("todo-input");
     const button = screen.getByTestId("todo-add-button");
@@ -28,5 +27,19 @@ describe("할 일 추가 폼 입력 테스트", () => {
     // then: 추가하기 버튼이 활성화된다
     expect(input).toHaveValue("테스트");
     expect(button).not.toBeDisabled();
+  });
+
+  it("입력값이 1글자 이상일 때 추가하기 버튼을 클릭하면 입력값이 초기화된다", () => {
+    // given: 입력값이 1글자 이상일 때
+    const input = screen.getByTestId("todo-input");
+    const button = screen.getByTestId("todo-add-button");
+
+    fireEvent.change(input, { target: { value: "테스트" } });
+
+    // when: 추가하기 버튼을 클릭하면
+    fireEvent.click(button);
+
+    // then: 입력값이 초기화된다
+    expect(input).toHaveValue("");
   });
 });
