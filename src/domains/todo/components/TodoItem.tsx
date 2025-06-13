00cd5a2ea79a0cf490fdbe4d0f59e1ae26f06d73
BangDori <qudwns8865@naver.com>
2025-06-14 00:08:16 +0900

    feat: 할 일 완료 상태 변경 api 연동

--- src/domains/todo/components/TodoItem.tsx
+++ src/domains/todo/components/TodoItem.tsx
@@ -5,17 +5,26 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { cn } from "@/utils/style";
 import { notify } from "@/utils/toastify";
+import { useCheckedTodoMutation } from "../api/useCheckedTodoMutation";
 import { useDeleteTodoMutation } from "../api/useDeleteTodoMutation";
-import { DeleteTodoModal } from "./DeleteTodoModal";
 import type { Todo } from "../types/todo";
+import { DeleteTodoModal } from "./DeleteTodoModal";
 
 export function TodoItem({ id, text, checked }: Todo) {
+  const { mutate: checkTodo } = useCheckedTodoMutation(id);
   const { mutate: deleteTodo } = useDeleteTodoMutation();
 
   return (
     <div className="flex items-center justify-between rounded-md border px-4 py-2 shadow-xs">
       <div className="flex items-center gap-3">
-        <Checkbox checked={checked} />
+        <Checkbox
+          checked={checked}
+          onCheckedChange={(checked) => {
+            if (typeof checked === "boolean") {
+              checkTodo(checked);
+            }
+          }}
+        />
         <Label
           className={cn(
             "flex-1 cursor-pointer select-none text-base transition-colors",
