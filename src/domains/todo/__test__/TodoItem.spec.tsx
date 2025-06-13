4febc7f50e756008f01b37ef89fe51bc40c44523
BangDori <44726494+BangDori@users.noreply.github.com>
2025-06-13 23:41:21 +0900

    할 일 삭제하기 API 연동 (#10)
    * docs: README 문서 업데이트
    * feat: 삭제 모달창 버튼 접근성 속성 추가
    * feat: react-toastify 추가
    * feat: autoClose 시간 수정
    * feat: 토스트 알림 메시지 유틸 함수 추가
    * feat: 할 일 삭제 뮤테이션 추가
    * feat: 할 일 삭제 api 연동
    * test: 삭제 모달창 테스트 QueryProvider 적용

--- src/domains/todo/__test__/TodoItem.spec.tsx
+++ src/domains/todo/__test__/TodoItem.spec.tsx
@@ -3,15 +3,21 @@ import userEvent from "@testing-library/user-event";
 import { OverlayProvider } from "overlay-kit";
 import { TodoItem } from "../components/TodoItem";
 import type { Todo } from "../types/todo";
+import { QueryProvider } from "@/queries/provider";
 
 const user = userEvent.setup();
+const Provider = ({ children }: { children: React.ReactNode }) => (
+  <QueryProvider>
+    <OverlayProvider>{children}</OverlayProvider>
+  </QueryProvider>
+);
 
 describe("할 일 항목 삭제 모달창 테스트", () => {
   const todo: Todo = { id: "1", text: "테스트 할 일", checked: false };
 
   it("삭제 아이콘 클릭 시 삭제 모달창이 렌더링된다", async () => {
     // given: 할 일 항목이 렌더링된 상태에서
-    render(<TodoItem {...todo} />, { wrapper: OverlayProvider });
+    render(<TodoItem {...todo} />, { wrapper: Provider });
 
     // when: 삭제 버튼을 클릭하면
     const deleteButton = screen.getByTestId("delete-todo-button");
--- src/domains/todo/components/DeleteTodoModal.tsx
+++ src/domains/todo/components/DeleteTodoModal.tsx
@@ -12,11 +12,13 @@ import { Button } from "@/components/ui/button";
 interface DeleteTodoModalProps {
   isOpen: boolean;
   onOpenChange: (open: boolean) => void;
+  onDelete: () => void;
 }
 
 export function DeleteTodoModal({
   isOpen,
   onOpenChange,
+  onDelete,
 }: DeleteTodoModalProps) {
   return (
     <Dialog open={isOpen} onOpenChange={onOpenChange}>
@@ -30,9 +32,24 @@ export function DeleteTodoModal({
         </DialogHeader>
         <DialogFooter>
           <DialogClose asChild>
-            <Button variant="outline">취소</Button>
+            <Button
+              type="button"
+              role="button"
+              aria-label="할 일 삭제 취소 버튼"
+              variant="outline"
+              className="cursor-pointer"
+            >
+              취소
+            </Button>
           </DialogClose>
-          <Button variant="destructive" onClick={() => {}}>
+          <Button
+            type="button"
+            role="button"
+            aria-label="할 일 삭제 버튼"
+            variant="destructive"
+            onClick={onDelete}
+            className="cursor-pointer"
+          >
             삭제
           </Button>
         </DialogFooter>
